997c01303145d9fb9d0539e25e9ce83eb9e7b6aa
Vladimir Rybant <vrybant@gmail.com>
2021-12-17 21:18:51 +0300

    update

--- lib/module.dart
+++ lib/module.dart
@@ -13,6 +13,8 @@ import 'package:sqflite/sqflite.dart';
 enum FileFormat { unbound, mysword, mybible }
 
 class Module {
+  Database? database;
+
   String filePath;
   String? fileName;
   FileFormat format = FileFormat.unbound;
@@ -38,6 +40,54 @@ class Module {
 
   Module(this.filePath) {
     fileName = basename(filePath);
+    final ext = extension(filePath);
+    print(ext);
+
+    if ((ext == ".mybible") || (ext == ".bbli")) {
+      format = FileFormat.mysword;
+    }
+    if (ext == ".SQLite3") {
+      format = FileFormat.mybible;
+    }
+
+    opendatabase();
+//  if !connected { return nil }
+  }
+
+  void opendatabase() async {
+    await deleteDatabase(filePath); // always getting a fresh copy from the asset
+
+    final exists = await databaseExists(filePath);
+
+    if (!exists) {
+      // Make sure the parent directory exists
+      try {
+        await Directory(dirname(filePath)).create(recursive: true);
+      } catch (_) {}
+
+      // Copy from asset
+      ByteData data = await rootBundle.load(join("assets", fileName));
+      List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+      await File(filePath).writeAsBytes(bytes, flush: true);
+    } else {
+      print("Opening existing database");
+    }
+
+    database = await openDatabase(filePath, readOnly: true);
+
+    print("***************\n");
+    print(filePath);
+
+    final List<Map<String, dynamic>> result = await database!.query("Details");
+
+    if (result.isNotEmpty) {
+      print(result[0]['Title']);
+      print(result[0]['Abbreviation']);
+      print(result[0]['Information']);
+    }
+
+    print(result.length);
+    print("~~~~~~~~~~~~~~~\n\n");
   }
 }
 
@@ -47,42 +97,5 @@ void main() async {
   const fileName = "en.kjv.bbl.unbound";
   final databasesPath = await getDatabasesPath();
   final path = join(databasesPath, fileName);
-
-  await deleteDatabase(path); // always getting a fresh copy from the asset
-  final exists = await databaseExists(path);
-
-  if (!exists) {
-    // Make sure the parent directory exists
-    try {
-      await Directory(dirname(databasesPath)).create(recursive: true);
-    } catch (_) {}
-
-    // Copy from asset
-    ByteData data = await rootBundle.load(join("assets", fileName));
-    List<int> bytes =
-        data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
-
-    // Write and flush the bytes written
-    await File(path).writeAsBytes(bytes, flush: true);
-  } else {
-    print("Opening existing database");
-  }
-
-  var database = openDatabase(path, readOnly: true);
-
-  print("***************\n");
-  print(path);
-
-  final db = await database;
-  final List<Map<String, dynamic>> result = await db.query("Details");
-
-  if (result.isNotEmpty) {
-    print(result[0]['Title']);
-    print(result[0]['Abbreviation']);
-    print(result[0]['Information']);
-  }
-  ;
-
-  print(result.length);
-  print("~~~~~~~~~~~~~~~\n\n");
+  final m = Module(path);
 }
