1a1a41950a6ce976ace3fb680ce3460e80a3bba4
Vladimir Rybant <vrybant@gmail.com>
2021-12-17 16:37:22 +0300

    class

--- lib/module.dart
+++ lib/module.dart
@@ -4,12 +4,42 @@
 // Even on iOS you could write a native plugin that get the asset file path
 // and directly open it in read-only mode. Android does not have such ability.
 
-import 'dart:async';
 import 'dart:io';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+
+enum FileFormat { unbound, mysword, mybible }
+
+class Module {
+  String filePath;
+  String? fileName;
+  FileFormat format = FileFormat.unbound;
+
+  String name = "";
+  String abbr = "";
+  String copyright = "";
+  String info = "";
+  String filetype = "";
+
+  String language = "en";
+  bool rightToLeft = true;
+
+  bool connected = false;
+  bool loaded = false;
+  bool strong = false;
+  bool embedded = false;
+  bool footnotes = false;
+  bool interlinear = false;
+  bool default_ = false;
+  bool accented = false;
+  bool favorite = true;
+
+  Module(this.filePath) {
+    fileName = basename(filePath);
+  }
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
