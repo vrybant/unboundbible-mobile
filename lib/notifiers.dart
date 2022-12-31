44f6c64fe4b195b72ed9144234426e3d2e4c9993
Vladimir Rybant <vrybant@gmail.com>
2022-12-31 19:54:47 +0300

    sort

--- lib/notifiers.dart
+++ lib/notifiers.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'core/tools.dart';
+import 'package:unboundbible/core/tools.dart';
 
 class ChapterNotifier extends StateNotifier<List<String>> {
   ChapterNotifier() : super([]) {
@@ -11,12 +11,6 @@ class ChapterNotifier extends StateNotifier<List<String>> {
   }
 }
 
-class ShelfNotifier extends StateNotifier<List<String>> {
-  ShelfNotifier() : super([]) {
-    state = tools!.get_Shelf();
-  }
-}
-
 class ShelfIndexNotifier extends StateNotifier<int> {
   ShelfIndexNotifier() : super(_initValue);
   static final _initValue = tools!.getCurrBibleIndex();
@@ -24,5 +18,11 @@ class ShelfIndexNotifier extends StateNotifier<int> {
   update(int value) {
     tools!.setCurrBible(value);
     state = value;
+  }
+}
+
+class ShelfNotifier extends StateNotifier<List<String>> {
+  ShelfNotifier() : super([]) {
+    state = tools!.get_Shelf();
   }
 }
--- lib/providers.dart
+++ lib/providers.dart
@@ -4,7 +4,7 @@ import 'package:unboundbible/notifiers.dart';
 final chapterProvider =
     StateNotifierProvider<ChapterNotifier, List<String>>((_) => ChapterNotifier());
 
-final shelfProvider = StateNotifierProvider<ShelfNotifier, List<String>>((_) => ShelfNotifier());
-
 final shelfIndexProvider =
     StateNotifierProvider<ShelfIndexNotifier, int>((_) => ShelfIndexNotifier());
+
+final shelfProvider = StateNotifierProvider<ShelfNotifier, List<String>>((_) => ShelfNotifier());
