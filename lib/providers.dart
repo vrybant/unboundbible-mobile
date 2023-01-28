d38b6f69f597642490984134fe25f5389a719dd6
Vladimir Rybant <vrybant@gmail.com>
2023-01-28 15:21:25 +0300

    update

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,22 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:unboundbible/core/tools.dart';
 import 'package:unboundbible/notifiers.dart';
 import 'package:unboundbible/states.dart';
 
-final currBibleProvider = StateNotifierProvider<CurrBibleNotifier, int>((ref) {
-  ref.watch(bibleProvider);
-  return CurrBibleNotifier();
-});
-
 final bibleProvider = StateNotifierProvider<BibleEngine, BibleState>((ref) {
+  ref.watch(shelfProvider);
   return BibleEngine();
 });
 
 final navigationIndexProvider = StateProvider<int>((_) => 0);
 
-final shelfProvider = StateProvider<List<String>>((_) => tools!.get_Shelf());
-
-final titlesProvider = StateProvider<List<String>>((ref) {
-  ref.watch(currBibleProvider);
-  return currBible!.getTitles();
-});
+final shelfProvider = StateNotifierProvider<ShelfNotifier, int>((_) => ShelfNotifier());
