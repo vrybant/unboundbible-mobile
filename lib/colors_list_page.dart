b2cd2521a3816cfce33b3de9db8cd052998083e2
Vladimir Rybant <vrybant@gmail.com>
2023-04-05 17:20:14 +0300

    update

---
+++ lib/colors_list_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ColorsListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "ColorsList",
+          ),
+          backgroundColor: Colors.green,
+        ),
+        body: Container(
+          color: Colors.white,
+          child: _buildList(),
+        ));
+  }
+
+  final List<int> materialIndices = [901, 800, 700, 600, 500, 400, 300, 200, 100, 50, 25, 5];
+
+  Widget _buildList() {
+    return ListView.builder(
+        itemCount: materialIndices.length,
+        itemBuilder: (BuildContext content, int index) {
+          int materialIndex = materialIndices[index];
+          return Container(
+            color: Colors.grey,
+            child: ListTile(
+              title: Text('$materialIndex', style: TextStyle(fontSize: 24.0)),
+              trailing: Icon(Icons.chevron_right),
+//            onTap: () => onPush?.call(materialIndex),
+            ),
+          );
+        });
+  }
+}
---
+++ lib/main_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'bible_page.dart';
+import 'colors_list_page.dart';
+import 'color_detail_page.dart';
+
+class BasicStatefulWidget extends StatefulWidget {
+  const BasicStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<BasicStatefulWidget> createState() => _BasicStatefulWidgetState();
+}
+
+class _BasicStatefulWidgetState extends State<BasicStatefulWidget> {
+  int selectedIndex = 0;
+
+  final pages = [
+//    BiblePage(),
+    ColorDetailPage(),
+    ColorsListPage(),
+    ColorDetailPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: selectedIndex,
+        children: pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.blue,
+        iconSize: 28,
+        selectedFontSize: 12,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
