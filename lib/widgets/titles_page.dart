ae53f099b3df2e385b3eacc495a241ed19864d02
Vladimir Rybant <vrybant@gmail.com>
2023-04-12 17:20:58 +0300

    update

--- lib/widgets/titles_page.dart
+++ lib/widgets/titles_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:unboundbible/core/tools.dart';
 
 class TitlesPage extends StatelessWidget {
   @override
@@ -18,29 +19,30 @@ class TitlesPage extends StatelessWidget {
   }
 
   Widget _buildList(BuildContext context) {
-    final lines = []; // ref.watch(bibleProvider).titles;
-    // final currBook = ref.watch(bibleProvider).book;
-    // final groupIndex = currBook - 1;
+    final lines = tools?.getTitles();
+    //final currBook = ref.watch(bibleProvider).book;
+    //final groupIndex = currBook - 1;
     return ListView.builder(
-        itemCount: lines.length,
+        itemCount: lines?.length,
         itemBuilder: (BuildContext content, int index) {
-          var line = lines[index];
+          var line = lines![index];
           return Container(
             color: Colors.white,
             child: ListTile(
-              dense: true,
-              title: Text(line, style: TextStyle(fontSize: 24.0)),
-              // onTap: () {
-              //   ref.read(bibleProvider.notifier).update(book: index + 1);
-              //   ref.context.go('/titles/chapters');
-              // }
-              //   leading: Radio<int>(
-              //       value: index,
-              //       groupValue: groupIndex,
-              //       onChanged: (value) {
-              //         ref.watch(bibleProvider.notifier).updateBook(value! + 1);
-              //       }),
-            ),
+                dense: true,
+                title: Text(line, style: TextStyle(fontSize: 24.0)),
+                onTap: () {
+                  print(line);
+//              ref.read(bibleProvider.notifier).update(book: index + 1);
+//              ref.context.go('/titles/chapters');
+                }
+                //   leading: Radio<int>(
+                //       value: index,
+                //       groupValue: groupIndex,
+                //       onChanged: (value) {
+                //         ref.watch(bibleProvider.notifier).updateBook(value! + 1);
+                //       }),
+                ),
           );
         });
   }
