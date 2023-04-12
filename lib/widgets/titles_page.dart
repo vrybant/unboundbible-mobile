ad6a5565f8eb2a942529224a657a4f08f464c9a9
Vladimir Rybant <vrybant@gmail.com>
2023-04-12 17:51:01 +0300

    update

--- lib/widgets/titles_page.dart
+++ lib/widgets/titles_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:unboundbible/controllers/chapter_controller.dart';
 import 'package:unboundbible/core/tools.dart';
 
 class TitlesPage extends StatelessWidget {
@@ -14,36 +15,28 @@ class TitlesPage extends StatelessWidget {
         ),
         body: Container(
           color: Colors.white,
-          child: _buildList(context),
+          child: _listView(context),
         ));
   }
 
-  Widget _buildList(BuildContext context) {
+  Widget _listView(BuildContext context) {
     final lines = tools?.getTitles();
-    //final currBook = ref.watch(bibleProvider).book;
-    //final groupIndex = currBook - 1;
     return ListView.builder(
-        itemCount: lines?.length,
-        itemBuilder: (BuildContext content, int index) {
-          var line = lines![index];
-          return Container(
-            color: Colors.white,
-            child: ListTile(
-                dense: true,
-                title: Text(line, style: TextStyle(fontSize: 24.0)),
-                onTap: () {
-                  print(line);
-//              ref.read(bibleProvider.notifier).update(book: index + 1);
-//              ref.context.go('/titles/chapters');
-                }
-                //   leading: Radio<int>(
-                //       value: index,
-                //       groupValue: groupIndex,
-                //       onChanged: (value) {
-                //         ref.watch(bibleProvider.notifier).updateBook(value! + 1);
-                //       }),
-                ),
-          );
-        });
+      itemCount: lines?.length,
+      itemBuilder: (BuildContext content, int index) {
+        var line = lines![index];
+        return Container(
+          color: Colors.white,
+          child: ListTile(
+            dense: true,
+            title: Text(line, style: TextStyle(fontSize: 24.0)),
+            onTap: () {
+              chapterController.update(book: index + 1);
+              context.go('/');
+            },
+          ),
+        );
+      },
+    );
   }
 }
